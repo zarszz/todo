5365c8ffd7095582a02a446e6f0a14e8d436e85e
zarszz <zarszzntunx@rocketmail.com>
2019-03-08 22:10:32 +0700

    add style

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,28 +1,41 @@
-#create method for todo_items database management
+# frozen_string_literal: true
+
+# create method for todo_items database management
 class TodoItemsController < ApplicationController
-    before_action :set_todo_list
+  before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
-    def create
-        @todo_item = @todo_list.todo_items.create(todo_item_params)
-        redirect_to @todo_list
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:succes] = 'Todo list item was deleted.'
+    else
+      flash[:error] = 'Todo List item could not be deleted'
     end
+    redirect_to @todo_list
+  end
 
-    def destroy
-        @todo_item = @todo_list.todo_item.find(params[:id])
-        if @todo_item.destroy
-            flash[:succes] = "Todo list item was deleted."
-        else
-            flash[:error] = "Todo List item could not be deleted"
-        end
-        redirect_to @todo_list
-    end
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: 'Todo item completed'
+  end
 
-    private
-        def set_todo_list
-            @todo_list = TodoList.find(params[:todo_list_id])
-        end
+  private
 
-        def todo_item_params
-            params[:todo_item].permit(:content)
-        end
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  def todo_item_params
+    params[:todo_item].permit(:content)
+  end
 end
